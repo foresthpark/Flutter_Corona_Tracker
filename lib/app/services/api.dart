afedc06963cde245c91d6a31f63f0b4edbfeda67
Forest Park <foresthpark@gmail.com>
2020-03-10 09:13:17 -0600

    retrive toekn success

--- lib/app/services/api.dart
+++ lib/app/services/api.dart
@@ -1,6 +1,8 @@
 import 'package:corona_tracker/app/services/api_keys.dart';
 import 'package:flutter/cupertino.dart';
 
+enum Endpoint { cases, casesSuspected, casesConfirmed, deaths, recovered }
+
 class API {
   API({@required this.apiKey});
   final String apiKey;
@@ -9,6 +11,7 @@ class API {
 
   static final String host = "apigw.nubentos.com";
   static final int port = 443;
+  static final String basePath = 't/nubentos.com/encovapi/1.0.0';
 
   Uri tokenUri() => Uri(
         scheme: 'https',
@@ -17,4 +20,18 @@ class API {
         path: 'token',
         queryParameters: {'grant_type': "client_credentials"},
       );
+
+  Uri endpointUri(Endpoint endpoint) => Uri(
+      scheme: 'https',
+      host: host,
+      port: port,
+      path: '$basePath/${_paths[endpoint]}');
+
+  static Map<Endpoint, String> _paths = {
+    Endpoint.cases: 'cases',
+    Endpoint.casesConfirmed: 'cases/confirmed',
+    Endpoint.casesSuspected: 'cases/suspected',
+    Endpoint.deaths: 'deaths',
+    Endpoint.recovered: 'recovered',
+  };
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:corona_tracker/app/services/api_dervice.dart';
 import 'package:flutter/material.dart';
+
+import 'app/services/api.dart';
 
 void main() => runApp(MyApp());
 
@@ -44,16 +47,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _accessToken = '';
 
-  void _incrementCounter() {
+  Future<void> _upsateAccessToken() async {
+    final apiService = APIService(API.sandbox());
+    final accessToken = await apiService.getAccessToken();
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _accessToken = accessToken;
     });
   }
 
@@ -95,14 +96,14 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              '$_accessToken',
               style: Theme.of(context).textTheme.display1,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _upsateAccessToken,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
